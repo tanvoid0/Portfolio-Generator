1fe525da1029d8939451eaff2de40e152cdf3300
Tanveer Hoque <tanveer_haque@yahoo.com>
2018-04-10 21:08:22 +0600

    v 1.4
    -Loader Added

--- resources/views/panel/includes/app.blade.php
+++ resources/views/panel/includes/app.blade.php
@@ -3,26 +3,32 @@
 <head>
 	@include('panel.includes.header')
 </head>
-<body class="hold-transition skin-blue sidebar-mini">
+<body class="hold-transition skin-blue sidebar-mini" onload="Loader()" style="margin:0;">
 <div class="wrapper">
-	@include('panel.includes.sidebar')
+  <div id="loader"></div>
+  @include('panel.includes.sidebar')
 
   <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
+    <div class="content-wrapper">
+      <div  id="loader-body" style="display:none;">
+
+      <!-- Content Header (Page header) -->
     @include('panel.includes.breadcumb')
 
     <!-- Main content -->
-    <section class="content">
-      @section('main-section')
-				@show
-    </section>
-    <!-- /.content -->
+      <section class="content">
+        @section('main-section')
+        @show
+      </section>
+      <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
   </div>
-  <!-- /.content-wrapper -->
+
 
 
 @include('panel.includes.footer');
-
+</div>
+@include('panel.includes.script');
 </body>
 </html>
