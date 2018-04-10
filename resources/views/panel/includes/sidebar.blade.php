8e44725b868e20fc1797d4c2856f4afea7bf26bb
Tanveer Hoque <tanveer_haque@yahoo.com>
2018-04-10 21:20:54 +0600

    Admin Panel name fix

--- resources/views/panel/includes/sidebar.blade.php
+++ resources/views/panel/includes/sidebar.blade.php
@@ -1,10 +1,10 @@
 <header class="main-header">
   <!-- Logo -->
-  <a href="{{ route('panel') }}" class="logo">
+  <a href="{{ route('dashboard') }}" class="logo">
     <!-- mini logo for sidebar mini 50x50 pixels -->
     <span class="logo-mini"><b>A</b>LT</span>
     <!-- logo for regular state and mobile devices -->
-    <span class="logo-lg"><b>User</b>Biography</span>
+    <span class="logo-lg"><b>Air</b>CV</span>
   </a>
   <!-- Header Navbar: style can be found in header.less -->
   <nav class="navbar navbar-static-top">
