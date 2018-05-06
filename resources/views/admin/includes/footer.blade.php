45f764003bc19fa6da81a8c615851fa067d46511
Tanveer Hoque <tanveer_haque@yahoo.com>
2018-05-06 16:48:30 +0600

    hmm

---
+++ resources/views/admin/includes/footer.blade.php
@@ -0,0 +1,24 @@
+
+<!-- Required Jquery -->
+<script type="text/javascript" src="{{ asset('assets/admin/js/jquery/jquery.min.js')  }}"></script>
+<script type="text/javascript" src="{{ asset('assets/admin/js/jquery-ui/jquery-ui.min.js')  }}"></script>
+<script type="text/javascript" src="{{ asset('assets/admin/js/popper.js/popper.min.js')  }}"></script>
+<script type="text/javascript" src="{{ asset('assets/admin/js/bootstrap/js/bootstrap.min.js')  }}"></script>
+<!-- jquery slimscroll js -->
+<script type="text/javascript" src="{{ asset('assets/admin/js/jquery-slimscroll/jquery.slimscroll.js')  }}"></script>
+<!-- modernizr js -->
+<script type="text/javascript" src="{{ asset('assets/admin/js/modernizr/modernizr.j')  }}s"></script>
+<!-- am chart -->
+<script src="{{ asset('assets/admin/pages/widget/amchart/amcharts.min.js')  }}"></script>
+<script src="{{ asset('assets/admin/pages/widget/amchart/serial.min.js')  }}"></script>
+<!-- Chart js -->
+<script type="text/javascript" src="{{ asset('assets/admin/js/chart.js/Chart.js')  }}"></script>
+<!-- Todo js -->
+<script type="text/javascript " src="{{ asset('assets/admin/pages/todo/todo.js ')  }}"></script>
+<!-- Custom js -->
+<script type="text/javascript" src="{{ asset('assets/admin/pages/dashboard/custom-dashboard.min.js')  }}"></script>
+<script type="text/javascript" src="{{ asset('assets/admin/js/script.js') }}"></script>
+<script type="text/javascript " src="{{ asset('assets/admin/js/SmoothScroll.js')  }}"></script>
+<script src="{{ asset('assets/admin/js/pcoded.min.js')  }}"></script>
+<script src="{{ asset('assets/admin/js/vartical-demo.js')  }}"></script>
+<script src="{{ asset('assets/admin/js/jquery.mCustomScrollbar.concat.min.js') }} "></script>
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,8 @@ Route::resource('panel/experience', 'ExperienceController')->middleware('auth');
 Route::resource('panel/work', 'WorkController')->middleware('auth');;
 Route::resource('panel/skill', 'SkillController')->middleware('auth');;
 Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout')->name('logout');
+
+
+Route::get('/admin', function(){
+    return view('admin.index');
+})->name('admin');
